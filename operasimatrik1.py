d74d15632f3624ac341bd3fab644441280408dc7
andri-heryandi <97411004+andri-heryandi@users.noreply.github.com>
2022-01-10 07:27:07 +0700

    Update operasimatrik1.py

--- operasimatrik1.py
+++ operasimatrik1.py
@@ -4,14 +4,14 @@ def penjumlahan(M1,M2):
     nbaris, nkolom = M1.shape
     for b in range(nbaris):
         for k in range(nkolom):
-            M3[b][k] = M1[b][k] + M2[b][k]
+            M3[b,k] = M1[b,k] + M2[b,k]
     return M3
 def pengurangan(M1,M2):
     M3 = np.empty(M1.shape)
     nbaris, nkolom = M1.shape
     for b in range(nbaris):
         for k in range(nkolom):
-            M3[b][k] = M1[b][k] - M2[b][k]
+            M3[b,k] = M1[b,k] - M2[b,k]
     return M3
 
 def perkalian(M1,M2):
@@ -21,7 +21,7 @@ def perkalian(M1,M2):
     for b in range(nbaris):
         for k in range(nkolom):
             for i in range(M1.shape[1]): # kolom matrik 1
-                M3[b][k] = M3[b][k] + M1[b][i] *  M2[i][k]
+                M3[b,k] = M3[b,k] + M1[b,i] *  M2[i,k]
     return M3
 # sample 
 A = np.array([[1, 2],
